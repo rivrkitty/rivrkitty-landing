c5fd4657df319f2120f1b712ad0319c9c6092b00
jegan <iamjegan@protonmail.com>
2021-11-26 16:26:43 +0530

    landing page buttons are added

--- src/main/components/LandingBanner.tsx
+++ src/main/components/LandingBanner.tsx
@@ -4,26 +4,23 @@ import { makeStyles } from "@mui/styles";
 import "@fontsource/roboto-mono";
 import { useMediaQuery } from "react-responsive";
 import Typography from "@mui/material/Typography";
+import { Button } from "@mui/material";
 import { defaultContentPadding } from "../../utils/theme";
 import FLOATINGKITTY from "../../assets/token/FLOATINGKITTY.png";
 import Box from "@mui/system/Box";
 
 const useStyles = makeStyles((theme) => ({
   bannerText: {
-    fontFamily: "Roboto Mono",
-    fontStyle: "Bold",
-    fontWeight: "700",
-    fontSize: "50px",
-    lineHeight: "66px",
-    align: "Left",
     verticalAlign: "Top",
     color: "#cccccc",
   },
   mobileBannerText: {
     textAlign: "center",
     margin: "0 auto",
+    fontSize: "36px",
+    lineHeight: "47px",
+    color: "#fff",
   },
-
   desktopRootCss: {
     backgroundImage: `linear-gradient(180deg, rgba(18, 26, 40, 0) 36.67%, #121A28 59.69%) , url(${
       getSingleAssetSrc("LANDINGIMAGE").default
@@ -32,7 +29,7 @@ const useStyles = makeStyles((theme) => ({
     width: "100%",
     minHeight: "1500px",
     display: "flex",
-    // justifyContent: "center",
+    flexDirection: "column",
   },
   mobileRootCss: {
     backgroundPositionX: "right",
@@ -54,16 +51,22 @@ const useStyles = makeStyles((theme) => ({
     animationName: "floating",
     animationDuration: "3s",
     animationIterationCount: "infinite",
-    animationTimingFunction: "ease-in-out"
+    animationTimingFunction: "ease-in-out",
   },
   moonRiverText: {
     color: "#EEAB47",
+  },
+  mobileBannerWrapper: {
+    padding: "0 5%",
+    textAlign: "center",
+    margin: "0 auto",
+    justifyContent: "end",
   },
 }));
 
 export default function LandingBanner() {
   const classes = useStyles();
-  const isTabletOrMobile = useMediaQuery({ maxWidth: 1024 });
+  const isTabletOrMobile = useMediaQuery({ maxWidth: 899 });
 
   return (
     <div
@@ -71,7 +74,12 @@ export default function LandingBanner() {
         classes.desktopRootCss
       }`}
     >
-      <Box className={classes.bannerWrapper} sx={{ ...defaultContentPadding }}>
+      <Box
+        className={`${isTabletOrMobile && classes.mobileBannerWrapper} ${
+          classes.bannerWrapper
+        }`}
+        sx={{ ...defaultContentPadding }}
+      >
         <Typography
           variant="h1"
           className={`${isTabletOrMobile && classes.mobileBannerText} ${
@@ -80,15 +88,51 @@ export default function LandingBanner() {
         >
           An Emerging Play&#8209;to&#8209;Earn KittyVerse Blending DeFi & NFTs
         </Typography>
-        <div className={classes.moonRiverText}>
+        <Box
+          sx={{
+            paddingTop: { xs: 1 },
+            paddingBottom: { xs: 1 },
+            wordBreak: "break-word",
+            color: "#EEAB47",
+          }}
+        >
           Moonriver Contract: 0xC2b0435276139731d82Ae2Fa8928c9b9De0761c1
+        </Box>
+        <div>
+          <Button
+            variant="contained"
+            sx={{
+              marginTop: 2,
+              marginRight: 2,
+              width: { xs: "100%", md: "inherit" },
+            }}
+          >
+            $BUY
+          </Button>
+          <Button
+            variant="contained"
+            sx={{
+              marginTop: 2,
+              marginRight: 2,
+              background: "#9969A6",
+              width: {
+                xs: "100%",
+                md: "inherit",
+              },
+            }}
+          >
+            CHART
+          </Button>
         </div>
       </Box>
       <Box className={classes.floatingRkittyAnim}>
-        <img style={{
-          height: "340px"
-        }}
-        src={FLOATINGKITTY} alt="Floating RivrKitty" />
+        <img
+          style={{
+            height: "340px",
+          }}
+          src={FLOATINGKITTY}
+          alt="Floating RivrKitty"
+        />
       </Box>
     </div>
   );
