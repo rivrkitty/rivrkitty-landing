69116028530946cbbe8ca2761f94568071e7c2d4
jegan <iamjegan@protonmail.com>
2021-11-28 22:57:21 +0530

    play and pause icon removed + autoplay & loop added

--- src/main/components/IntroVideo.tsx
+++ src/main/components/IntroVideo.tsx
@@ -1,7 +1,5 @@
 import { IconButton } from "@mui/material";
 import { Box } from "@mui/system";
-import PlayCircleIcon from "@mui/icons-material/PlayCircle";
-import PauseCircleIcon from "@mui/icons-material/PauseCircle";
 import VolumeOffIcon from "@mui/icons-material/VolumeOff";
 import VolumeUpIcon from "@mui/icons-material/VolumeUp";
 import React from "react";
@@ -17,31 +15,13 @@ export default function IntroVideo() {
   const videoRef = React.useRef<HTMLVideoElement | null>(null);
   const { ref, inView } = useInView({ threshold: 0.2 });
   const [isMuted, setIsMuted] = React.useState(true);
-  const [isPaused, setIsPaused] = React.useState(false);
   const [isHover, setIsHover] = React.useState(false);
-
-  React.useEffect(() => {
-    if (inView) {
-      videoRef.current?.play();
-    } else {
-      videoRef.current?.pause();
-    }
-  }, [inView]);
 
   const handleMouseEnter = () => {
     setIsHover(true);
   };
   const handleMouseLeave = () => {
     setIsHover(false);
-  };
-  const handlePlayClick = (event: any) => {
-    if (videoRef.current?.paused === true) {
-      videoRef.current?.play();
-      setIsPaused(false);
-    } else {
-      videoRef.current?.pause();
-      setIsPaused(true);
-    }
   };
   const handleVolumeClick = (event: any) => {
     const vid = videoRef.current;
@@ -72,6 +52,8 @@ export default function IntroVideo() {
             ref={videoRef}
             component="video"
             sx={{ maxWidth: "100%", maxHeight: 600 }}
+            autoPlay
+            loop
             muted
           >
             <source src="videos/intro.m4v" type="video/mp4" />
@@ -86,7 +68,7 @@ export default function IntroVideo() {
               left: 0,
               right: 0,
               bottom: 0,
-              backgroundColor: isHover || isPaused ? "#000000aa" : "#00000000",
+              backgroundColor: isHover ? "#000000aa" : "#00000000",
               transition: "background-color 0.5s",
             }}
             onMouseEnter={handleMouseEnter}
@@ -96,22 +78,15 @@ export default function IntroVideo() {
               size="large"
               sx={{
                 marginRight: 1,
-                opacity: isHover || isPaused ? "100%" : "0%",
+                opacity: isHover ? "100%" : "0%",
                 transition: "opacity 0.5s",
               }}
-              onClick={handlePlayClick}
-            >
-              {isPaused ? (
-                <PlayCircleIcon sx={iconSx} />
-              ) : (
-                <PauseCircleIcon sx={iconSx} />
-              )}
-            </IconButton>
+            ></IconButton>
             <IconButton
               size="large"
               sx={{
                 marginLeft: 1,
-                opacity: isHover || isPaused ? "100%" : "0%",
+                opacity: isHover ? "100%" : "0%",
                 transition: "opacity 0.5s",
               }}
               onClick={handleVolumeClick}
